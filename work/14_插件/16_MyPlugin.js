16159427ec1ed06a84aeff2cb77551f6654e3aa6
zxfjd3g <258147149@qq.com>
2017-02-18 11:05:58 +0800

    update

--- work/14_插件/16_MyPlugin.js
+++ work/14_插件/16_MyPlugin.js
@@ -1,20 +1,22 @@
-/**
- * Created by xfzhang on 2016/8/25.
- */
-MyPlugin = {};
-MyPlugin.install = function (Vue, options) {
-  // 1. 添加全局方法或属性
-  Vue.myGlobalMethod = function () {
-    alert('Vue myGlobalMethod()');
-  }
-  // 2. 添加全局资源
-  Vue.directive('my-directive', {
-    update: function (value) {
-      this.el.innerHTML = "MyPlugin my-directive " + value
+
+(function (window) {
+  //创建插件对象
+  const MyPlugin = {}
+  //添加install方法
+  MyPlugin.install = function (Vue, options) {
+    // 1. 添加全局方法或属性
+    Vue.myGlobalMethod = function () {
+      console.log('Vue myGlobalMethod()')
     }
-  });
-  // 3. 添加实例方法
-  Vue.prototype.$myMethod = function () {
-    alert('vue $myMethod()');
+    // 2. 添加全局资源: v-my-directive='xxx'
+    Vue.directive('my-directive', function (value) {
+      this.el.innerHTML = value.toUpperCase()
+    })
+    // 3. 添加实例方法
+    Vue.prototype.$myMethod = function () {
+      console.log('vue对象$myMethod()')
+    }
   }
-};
+  //暴露插件对象
+  window.MyPlugin = MyPlugin
+})(window)
